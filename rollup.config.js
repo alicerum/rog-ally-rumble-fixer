ebdd14d7c4811f9f6364b1fd49bafcdd9d373d10
Alice Rum <wyvie@wyvie.org>
2026-03-05 00:21:18 +0100

    another init fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,5 @@
-import typescript from "@rollup/plugin-typescript";
-import json from "@rollup/plugin-json";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import replace from "@rollup/plugin-replace";
-import importCss from "rollup-plugin-import-css";
+import deckyPlugin from "@decky/rollup";
 
-export default {
-  input: "src/index.tsx",
-  output: {
-    file: "dist/index.js",
-    format: "esm",
-    sourcemap: false,
-  },
-  plugins: [
-    replace({
-      preventAssignment: true,
-      "process.env.NODE_ENV": JSON.stringify("production"),
-    }),
-    importCss(),
-    nodeResolve({
-      browser: true,
-      preferBuiltins: false,
-    }),
-    commonjs(),
-    json(),
-    typescript({
-      tsconfig: "./tsconfig.json",
-    }),
-  ],
-  external: ["react", "react-dom", "@decky/api", "@decky/ui"],
-};
+export default deckyPlugin({
+  // Plugin is external to Decky Loader
+});
